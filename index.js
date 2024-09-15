e9696b7e9dbb3332a828a0aa604ac497317cc667
mrfhmd7 <mrf.hmd7@gmail.com>
2024-09-15 16:13:01 +0600

    get news through id

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 
@@ -12,6 +14,18 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
      res.send(categories);
+})
+
+
+app.get('/news', (req, res) => {
+     res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+     const id = req.params.id;
+     console.log(id);
+     const selectedNews = news.find(n => n._id === id);
+     res.send(selectedNews);
 })
 
 app.listen(port, () => {
